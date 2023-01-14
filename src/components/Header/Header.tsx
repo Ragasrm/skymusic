05bf95c9b369745ef5d7cc39da63bf0beb7766c9
Ragavendiran S <Srm482@gmail.com>
2023-01-14 23:14:16 +0530

    feat: completed tab bar ui development

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { AppBar, Box, CssBaseline, Toolbar, Typography } from '@mui/material'
+import './header.css'
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <Box sx={{ display: 'flex' }}>
+    <CssBaseline />
+    <AppBar component="nav">
+      <Toolbar className='header-toolbar'>
+        <Typography
+          variant="h6"
+          className='header-title'         
+        >
+          Sky Musiq
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    </Box>
+  )
+}
+
+export default Header
