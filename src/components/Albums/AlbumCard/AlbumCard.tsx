d3defead8b201a45d8f98244652bd6e93bc6f9ec
Ragavendiran S <Srm482@gmail.com>
2023-01-15 12:46:51 +0530

    feat: responsive design code

---
+++ src/components/Albums/AlbumCard/AlbumCard.tsx
@@ -0,0 +1,10 @@
+import './AlbumCard.css'
+type Props = {}
+
+export default function AlbumCard({ }: Props) {
+    return (
+        <div className='album-card'>
+            Ragav
+        </div>
+    )
+}
--- src/components/Albums/AlbumContent/AlbumContent.tsx
+++ src/components/Albums/AlbumContent/AlbumContent.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, CardActions, CardContent, Grid, Typography } from '@mui/material'
+import {Grid } from '@mui/material'
+import AlbumCard from '../AlbumCard/AlbumCard'
 import './AlbumContent.css'
 
 type Props = {}
@@ -7,68 +8,15 @@ function AlbumContent({}: Props) {
   return (
     <div className='album-content'>
 
-        
-          <Grid container spacing={2}>
 
-          {Array.from(Array(6)).map((_, index) => (
-          <Grid item xs={12} sm={4} md={3} lg={3} xl={3} key={index}>
-          <Card sx={{ minWidth: 275 }}>
-              <CardContent>
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                      Word of the Day
-                  </Typography>
-                  <Typography variant="h5" component="div">
-                      be{"bull"}nev{"bull"}o{"bull"}lent
-                  </Typography>
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                      adjective
-                  </Typography>
-                  <Typography variant="body2">
-                      well meaning and kindly.
-                      <br />
-                      {'"a benevolent smile"'}
-                  </Typography>
-              </CardContent>
-              <CardActions>
-                  <Button size="small">Learn More</Button>
-              </CardActions>
-          </Card>
-      </Grid>
-        ))}
-              {/* <Grid item xs={12} sm={4} md={3} lg={3} xl={3}>
-                  <Card sx={{ minWidth: 275 }}>
-                      <CardContent>
-                          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                              Word of the Day
-                          </Typography>
-                          <Typography variant="h5" component="div">
-                              be{"bull"}nev{"bull"}o{"bull"}lent
-                          </Typography>
-                          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                              adjective
-                          </Typography>
-                          <Typography variant="body2">
-                              well meaning and kindly.
-                              <br />
-                              {'"a benevolent smile"'}
-                          </Typography>
-                      </CardContent>
-                      <CardActions>
-                          <Button size="small">Learn More</Button>
-                      </CardActions>
-                  </Card>
-              </Grid>
-              <Grid item xs={12} sm={4} md={3} lg={3} xl={3}>
-              <div>card two</div>
-              </Grid>
-              <Grid item xs={12} sm={4} md={3} lg={3} xl={3}>
-              <div>card three</div>
-              </Grid>
-              <Grid item xs={12} sm={4} md={3} lg={3} xl={3}>
-              <div>card Four</div>
-              </Grid> */}
-          </Grid>
-    </div>
+          <div className='album-card-container'>
+              {Array.from(Array(4)).map((_, index) => (
+
+                  <AlbumCard />
+
+              ))}
+          </div>
+    </div>  
   )
 }
 
